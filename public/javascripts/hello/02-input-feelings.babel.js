c5f0fdf4c0e6f18218f528040353b79d8ea47edb
asemahle <aidan.mahler13@gmail.com>
2020-03-14 19:43:39 -0400

    trying to fix for ios 9.3 -.-

--- public/javascripts/hello/02-input-feelings.babel.js
+++ public/javascripts/hello/02-input-feelings.babel.js
@@ -9,51 +9,50 @@ function run() {
 
     document.getElementById("restart").addEventListener("click", function () {
         window.location.assign("/hello/01-hello");
-    }); // selection area controls
-
-    var point = {
-        x: 0,
-        y: 0
-    };
-    var nextElem = document.getElementById("next");
-    var eventElem = document.getElementById("selection-events");
-    eventElem.addEventListener('update', function (e) {
-        point.x = e.detail.x;
-        point.y = e.detail.y;
-        nextElem.classList.remove("hide");
-    }); // go forward on 'done'
-
-    document.getElementById("next").addEventListener("click", function () {
-        function reqListener() {
-            window.location.assign("/hello/03-thank-you?name=" + encodeURIComponent(name));
-        }
-
-        var xmlhttp = new XMLHttpRequest(); // new HttpRequest instance
-
-        var theUrl = "/data";
-        xmlhttp.addEventListener("load", reqListener);
-        xmlhttp.open("POST", theUrl);
-        xmlhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-        xmlhttp.send(JSON.stringify({
-            displayID: name,
-            x: point.x,
-            y: point.y
-        })); // fetch('/data', {
-        //     method: 'POST',
-        //     headers: {
-        //         'Content-Type': 'application/json',
-        //     },
-        //     body: JSON.stringify({
-        //         displayID: name,
-        //         x: point.x,
-        //         y: point.y
-        //     }),
-        // }).then((data) => {
-        //     window.location.assign("/hello/03-thank-you?" + encodeGetParams({name: name}));
-        // }).catch((error) => {
-        //     window.location.assign("/hello/03-thank-you?" + encodeGetParams({name: name}));
-        // });
-    }); // go back to intro after timeout
-
-    SetExitTimer();
+    }); // // selection area controls
+    // let point = {x: 0, y: 0};
+    // let nextElem = document.getElementById("next");
+    // let eventElem = document.getElementById("selection-events");
+    // eventElem.addEventListener('update', function (e) {
+    //     point.x = e.detail.x;
+    //     point.y = e.detail.y;
+    //     nextElem.classList.remove("hide");
+    // });
+    //
+    // // go forward on 'done'
+    // document.getElementById("next").addEventListener("click", function () {
+    //     function reqListener () {
+    //         window.location.assign("/hello/03-thank-you?name=" + encodeURIComponent(name));
+    //     }
+    //
+    //     let xmlhttp = new XMLHttpRequest();   // new HttpRequest instance
+    //     let theUrl = "/data";
+    //     xmlhttp.addEventListener("load", reqListener);
+    //     xmlhttp.open("POST", theUrl);
+    //     xmlhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    //     xmlhttp.send(JSON.stringify({
+    //         displayID: name,
+    //         x: point.x,
+    //         y: point.y
+    //     }));
+    //
+    //     // fetch('/data', {
+    //     //     method: 'POST',
+    //     //     headers: {
+    //     //         'Content-Type': 'application/json',
+    //     //     },
+    //     //     body: JSON.stringify({
+    //     //         displayID: name,
+    //     //         x: point.x,
+    //     //         y: point.y
+    //     //     }),
+    //     // }).then((data) => {
+    //     //     window.location.assign("/hello/03-thank-you?" + encodeGetParams({name: name}));
+    //     // }).catch((error) => {
+    //     //     window.location.assign("/hello/03-thank-you?" + encodeGetParams({name: name}));
+    //     // });
+    // });
+    //
+    // // go back to intro after timeout
+    // SetExitTimer();
 }
