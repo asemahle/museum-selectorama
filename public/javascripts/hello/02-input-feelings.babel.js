7befc37006cb26f30b17f2ef9833ef1ab0590ee6
asemahle <aidan.mahler13@gmail.com>
2020-03-14 19:36:08 -0400

    trying to fix for ios 9.3 -.-

---
+++ public/javascripts/hello/02-input-feelings.babel.js
@@ -0,0 +1,59 @@
+"use strict";
+
+var url = new URL(window.location.href);
+var name = url.searchParams.get("name");
+
+function run() {
+    // set the name
+    document.getElementById("name").innerText = name; // go back on 'restart'
+
+    document.getElementById("restart").addEventListener("click", function () {
+        window.location.assign("/hello/01-hello");
+    }); // selection area controls
+
+    var point = {
+        x: 0,
+        y: 0
+    };
+    var nextElem = document.getElementById("next");
+    var eventElem = document.getElementById("selection-events");
+    eventElem.addEventListener('update', function (e) {
+        point.x = e.detail.x;
+        point.y = e.detail.y;
+        nextElem.classList.remove("hide");
+    }); // go forward on 'done'
+
+    document.getElementById("next").addEventListener("click", function () {
+        function reqListener() {
+            window.location.assign("/hello/03-thank-you?name=" + encodeURIComponent(name));
+        }
+
+        var xmlhttp = new XMLHttpRequest(); // new HttpRequest instance
+
+        var theUrl = "/data";
+        xmlhttp.addEventListener("load", reqListener);
+        xmlhttp.open("POST", theUrl);
+        xmlhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+        xmlhttp.send(JSON.stringify({
+            displayID: name,
+            x: point.x,
+            y: point.y
+        })); // fetch('/data', {
+        //     method: 'POST',
+        //     headers: {
+        //         'Content-Type': 'application/json',
+        //     },
+        //     body: JSON.stringify({
+        //         displayID: name,
+        //         x: point.x,
+        //         y: point.y
+        //     }),
+        // }).then((data) => {
+        //     window.location.assign("/hello/03-thank-you?" + encodeGetParams({name: name}));
+        // }).catch((error) => {
+        //     window.location.assign("/hello/03-thank-you?" + encodeGetParams({name: name}));
+        // });
+    }); // go back to intro after timeout
+
+    SetExitTimer();
+}
