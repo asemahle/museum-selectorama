b03c31a4c54ae538617f3cccfd1961fc43835563
asemahle <aidan.mahler13@gmail.com>
2020-03-14 20:12:41 -0400

    trying to fix for ios 9.3 -.-

--- public/javascripts/selection.babel.js
+++ public/javascripts/selection.babel.js
@@ -35,11 +35,13 @@
 
             $e.append(_$dot);
         }
-        document.body.innerText = pos.pageX + " || " + pos.pageY;
 
         var $dot = $e.getElementsByClassName("active-dot")[0];
         var dotBounds = $dot.getBoundingClientRect();
         var bounds = $e.getBoundingClientRect();
+
+        document.body.innerText = pos.pageX + " || " + pos.pageY;
+
         var dotPos = {
             x: clamp(pos.pageX - bounds.x, 0, bounds.width) - dotBounds.width / 2,
             y: clamp(pos.pageY - bounds.y - window.scrollY, 0, bounds.height) - dotBounds.height / 2
