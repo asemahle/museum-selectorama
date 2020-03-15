6538d3ba9223de2e28fcb647c866543812204348
asemahle <aidan.mahler13@gmail.com>
2020-03-14 21:11:06 -0400

    trying to fix for ios 9.3 -.-

--- public/javascripts/utils.js
+++ public/javascripts/utils.js
@@ -1,5 +1,5 @@
 let encodeGetParams = function (p) {
-    return Object.entries(p).map(kv => kv.map(encodeURIComponent).join("=")).join("&");
+    return Object.keys(p).map(k => [encodeURIComponent(k), encodeURIComponent(p[k])].join("=")).join("&");
 };
 
 let clamp = function (val, min, max) {
