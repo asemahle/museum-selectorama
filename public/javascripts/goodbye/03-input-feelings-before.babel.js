075ff76f6a4ff169c3fb165b63fd5e69a4577945
asemahle <aidan.mahler13@gmail.com>
2020-03-14 21:32:08 -0400

    trying to fix for ios 9.3 -.-

---
+++ public/javascripts/goodbye/03-input-feelings-before.babel.js
@@ -0,0 +1,28 @@
+"use strict";
+
+// go back on 'restart'
+document.getElementById("restart").addEventListener("click", function () {
+    window.location.assign("/goodbye/01-hello");
+}); // selection area controls
+
+var point = {
+    x: 0,
+    y: 0
+};
+var nextElem = document.getElementById("next");
+var eventElem = document.getElementById("selection-events");
+eventElem.addEventListener('update', function (e) {
+    point.x = e.detail.x;
+    point.y = e.detail.y;
+    nextElem.classList.remove("hide");
+}); // go forward on 'done'
+
+document.getElementById("next").addEventListener("click", function () {
+    var params = {
+        x: point.x,
+        y: point.y
+    };
+    window.location.assign("/goodbye/04-input-feelings-after?x=" + encodeURIComponent(point.x) + "&y=" + encodeURIComponent(point.y));
+}); // go back to intro after timeout
+
+SetExitTimer();
---
+++ public/javascripts/goodbye/03-input-feelings-before.js
@@ -0,0 +1,26 @@
+// go back on 'restart'
+document.getElementById("restart").addEventListener("click", function() {
+    window.location.assign("/goodbye/01-hello");
+});
+
+// selection area controls
+let point = {x: 0, y: 0};
+let nextElem = document.getElementById("next");
+let eventElem = document.getElementById("selection-events");
+eventElem.addEventListener('update', function(e) {
+    point.x = e.detail.x;
+    point.y = e.detail.y;
+    nextElem.classList.remove("hide");
+});
+
+// go forward on 'done'
+document.getElementById("next").addEventListener("click", function() {
+    let params = {
+        x: point.x,
+        y: point.y
+    };
+    window.location.assign("/goodbye/04-input-feelings-after?x=" + encodeURIComponent(point.x) + "&y=" + encodeURIComponent(point.y));
+});
+
+// go back to intro after timeout
+SetExitTimer();
