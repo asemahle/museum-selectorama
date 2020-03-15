46ead12d6b661b5662225b1b384c9cd57aa1daf4
asemahle <aidan.mahler13@gmail.com>
2020-03-14 21:36:01 -0400

    trying to fix for ios 9.3 -.-

---
+++ public/javascripts/goodbye/04-input-feelings-after.babel.js
@@ -0,0 +1,57 @@
+"use strict";
+
+var s = window.location.href;
+var oldX = decodeURIComponent(s.substring(s.indexOf("?x=") + 3, s.indexOf("&")));
+var oldY = decodeURIComponent(s.substr(s.indexOf("&y=") + 3)); // go back on 'restart'
+
+document.getElementById("restart").addEventListener("click", function () {
+    window.location.assign("/goodbye/01-hello");
+}); // selection area controls
+
+var point = {
+    x: 0,
+    y: 0
+};
+var nextElem = document.getElementById("next");
+var eventElem = document.getElementById("selection-events");
+eventElem.addEventListener('update', function (e) {
+    point.x = e.detail.x;
+    point.y = e.detail.y;
+    nextElem.classList.remove("hide");
+}); // go forward on 'done'
+
+document.getElementById("next").addEventListener("click", function () {
+    function reqListener() {
+        window.location.assign("/goodbye/05-thank-you");
+    }
+
+    var xmlhttp = new XMLHttpRequest(); // new HttpRequest instance
+
+    var theUrl = '/data/fullAdd';
+    xmlhttp.addEventListener("load", reqListener);
+    xmlhttp.open("POST", theUrl);
+    xmlhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    xmlhttp.send(JSON.stringify({
+        x1: oldX,
+        y1: oldY,
+        x2: point.x,
+        y2: point.y
+    })); // fetch('/data/fullAdd', {
+    //     method: 'POST',
+    //     headers: {
+    //         'Content-Type': 'application/json',
+    //     },
+    //     body: JSON.stringify({
+    //         x1: oldX,
+    //         y1: oldY,
+    //         x2: point.x,
+    //         y2: point.y
+    //     }),
+    // }).then((data) => {
+    //     window.location.assign("/goodbye/05-thank-you");
+    // }).catch((error) => {
+    //     window.location.assign("/goodbye/05-thank-you");
+    // });
+}); // go back to intro after timeout
+
+SetExitTimer();
---
+++ public/javascripts/goodbye/04-input-feelings-after.js
@@ -0,0 +1,57 @@
+let s = window.location.href;
+let oldX = decodeURIComponent(s.substring(s.indexOf("?x=") + 3, s.indexOf("&")));
+let oldY = decodeURIComponent(s.substr(s.indexOf("&y=") + 3));
+
+// go back on 'restart'
+document.getElementById("restart").addEventListener("click", function() {
+    window.location.assign("/goodbye/01-hello");
+});
+
+// selection area controls
+let point = {x: 0, y: 0};
+let nextElem = document.getElementById("next");
+let eventElem = document.getElementById("selection-events");
+eventElem.addEventListener('update', function(e) {
+    point.x = e.detail.x;
+    point.y = e.detail.y;
+    nextElem.classList.remove("hide");
+});
+
+// go forward on 'done'
+document.getElementById("next").addEventListener("click", function() {
+    function reqListener () {
+        window.location.assign("/goodbye/05-thank-you");
+    }
+
+    let xmlhttp = new XMLHttpRequest();   // new HttpRequest instance
+    let theUrl = '/data/fullAdd';
+    xmlhttp.addEventListener("load", reqListener);
+    xmlhttp.open("POST", theUrl);
+    xmlhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    xmlhttp.send(JSON.stringify({
+        x1: oldX,
+        y1: oldY,
+        x2: point.x,
+        y2: point.y
+    }));
+
+    // fetch('/data/fullAdd', {
+    //     method: 'POST',
+    //     headers: {
+    //         'Content-Type': 'application/json',
+    //     },
+    //     body: JSON.stringify({
+    //         x1: oldX,
+    //         y1: oldY,
+    //         x2: point.x,
+    //         y2: point.y
+    //     }),
+    // }).then((data) => {
+    //     window.location.assign("/goodbye/05-thank-you");
+    // }).catch((error) => {
+    //     window.location.assign("/goodbye/05-thank-you");
+    // });
+});
+
+// go back to intro after timeout
+SetExitTimer();
