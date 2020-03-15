8186004416076b1f0c96d29bc14627c145da06c7
asemahle <aidan.mahler13@gmail.com>
2020-03-14 21:26:34 -0400

    trying to fix for ios 9.3 -.-

---
+++ public/javascripts/goodbye/02-input-feelings.babel.js
@@ -0,0 +1,57 @@
+"use strict";
+
+var s = window.location.href;
+var id = decodeURIComponent(s.substring(s.indexOf("?id=") + 4, s.indexOf("&")));
+var name = decodeURIComponent(s.substr(s.indexOf("&name=") + 6)); // set the name
+
+document.getElementById("name").innerText = name; // go back on 'restart'
+
+document.getElementById("restart").addEventListener("click", function () {
+    window.location.assign("/goodbye/01-hello");
+}); // selection area controls
+
+var point = {
+    x: 0,
+    y: 0
+};
+var nextElem = document.getElementById("next");
+var eventElem = document.getElementById("selection-events");
+eventElem.addEventListener('update', function (e) {
+    point.x = e.detail.x;
+    point.y = e.detail.y;
+    nextElem.classList.remove("hide");
+}); // go forward on 'done'
+
+document.getElementById("next").addEventListener("click", function () {
+    function reqListener() {
+        window.location.assign("/goodbye/05-thank-you");
+    }
+
+    var xmlhttp = new XMLHttpRequest(); // new HttpRequest instance
+
+    var theUrl = '/data/complete/' + id;
+    xmlhttp.addEventListener("load", reqListener);
+    xmlhttp.open("POST", theUrl);
+    xmlhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    xmlhttp.send(JSON.stringify({
+        userID: id,
+        x: point.x,
+        y: point.y
+    })); // fetch('/data/complete/' + id, {
+    //     method: 'POST',
+    //     headers: {
+    //         'Content-Type': 'application/json',
+    //     },
+    //     body: JSON.stringify({
+    //         userID: id,
+    //         x: point.x,
+    //         y: point.y
+    //     }),
+    // }).then((data) => {
+    //     window.location.assign("/goodbye/05-thank-you");
+    // }).catch((error) => {
+    //     window.location.assign("/goodbye/05-thank-you");
+    // });
+}); // go back to intro after timeout
+
+SetExitTimer();
---
+++ public/javascripts/goodbye/02-input-feelings.js
@@ -0,0 +1,58 @@
+let s = window.location.href;
+let id = decodeURIComponent(s.substring(s.indexOf("?id=") + 4, s.indexOf("&")));
+let name = decodeURIComponent(s.substr(s.indexOf("&name=") + 6));
+
+// set the name
+document.getElementById("name").innerText = name;
+
+// go back on 'restart'
+document.getElementById("restart").addEventListener("click", function() {
+    window.location.assign("/goodbye/01-hello");
+});
+
+// selection area controls
+let point = {x: 0, y: 0};
+let nextElem = document.getElementById("next");
+let eventElem = document.getElementById("selection-events");
+eventElem.addEventListener('update', function(e) {
+    point.x = e.detail.x;
+    point.y = e.detail.y;
+    nextElem.classList.remove("hide");
+});
+
+// go forward on 'done'
+document.getElementById("next").addEventListener("click", function() {
+    function reqListener () {
+        window.location.assign("/goodbye/05-thank-you");
+    }
+
+    let xmlhttp = new XMLHttpRequest();   // new HttpRequest instance
+    let theUrl = '/data/complete/' + id;
+    xmlhttp.addEventListener("load", reqListener);
+    xmlhttp.open("POST", theUrl);
+    xmlhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    xmlhttp.send(JSON.stringify({
+        userID: id,
+        x: point.x,
+        y: point.y
+    }));
+
+    // fetch('/data/complete/' + id, {
+    //     method: 'POST',
+    //     headers: {
+    //         'Content-Type': 'application/json',
+    //     },
+    //     body: JSON.stringify({
+    //         userID: id,
+    //         x: point.x,
+    //         y: point.y
+    //     }),
+    // }).then((data) => {
+    //     window.location.assign("/goodbye/05-thank-you");
+    // }).catch((error) => {
+    //     window.location.assign("/goodbye/05-thank-you");
+    // });
+});
+
+// go back to intro after timeout
+SetExitTimer();
